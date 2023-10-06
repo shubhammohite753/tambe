50b67922a0c67017d531f09a3617b02898fe1916
shubhammohite753 <117722147+shubhammohite753@users.noreply.github.com>
2023-10-06 15:50:35 +0530

    first

---
+++ src/app/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-primary text-center text-white header'>
+        E2E Flow
+    </div>
+  )
+}
+
+export default Header;
